aa880635197714d58d09248e6b334d744b8b4069
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-09-30 16:16:49 +0400

    Basic lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-use syntax;
+use syntax::Lex;
 
-fn main() {}
+use std::fs;
+
+fn main() {
+    let source = fs::read_to_string("source.mellow").unwrap();
+    let lex = Lex::new(source.chars().peekable());
+    for token in lex {
+        println!("{token:?}");
+    }
+}
