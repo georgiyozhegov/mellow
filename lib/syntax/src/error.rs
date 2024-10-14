ecbd25b44673d0ea26d34c5343e53b279e5b83d3
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-10-14 12:27:01 +0400

    Better description of grammar errors

--- lib/syntax/src/error.rs
+++ lib/syntax/src/error.rs
@@ -1,5 +1,10 @@
+use crate::Token;
+
 #[derive(Debug)]
 pub enum SyntaxError {
     InvalidCharacter(char),
-    Grammar(String),
+    Grammar {
+        expected: &'static str,
+        found: Option<Token>,
+    },
 }
