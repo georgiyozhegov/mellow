21d9926534ee5bebb5915e7c514ab00541cec550
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-11-11 19:44:04 +0400

    rm: assembly generation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use std::{fs, process::exit};
 
 use syntax::{Lex, Parse};
-use assembly::Assembly;
 
 fn main() {
     let source = fs::read_to_string("source.mellow").unwrap();
     let lex = Lex::new(source.chars().peekable());
     let parse = Parse::new(lex.peekable());
-    let assembly = Assembly::new(parse);
-    for block in assembly {
-        println!("{block:?}");
-    }
-    /*
+
     for statement in parse {
         let statement = match statement {
             Ok(statement) => statement,
@@ -22,5 +17,4 @@ fn main() {
         };
         println!("{statement:?}");
     }
-    */
 }
