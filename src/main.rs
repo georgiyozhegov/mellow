7a33a63081c1442c3ec771baf86cfb0965876b1e
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-12-08 19:49:17 +0400

    Basic IR

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::{fs, process::exit};
 
-use ir::constant_fold;
-use syntax::{Lex, Parse};
+use ir::{constant_fold, ir::ir};
+use syntax::{Lex, Parse, SyntaxError};
 
 fn main() {
     let source = fs::read_to_string("source.mellow").unwrap();
     let lex = Lex::new(source.chars().peekable());
     let parse = Parse::new(lex.peekable());
 
+    let ir = ir(parse.collect::<Result<Vec<_>, SyntaxError>>().unwrap());
+
+    for block in ir {
+        println!("{block:?}");
+    }
+
+    /*
     for statement in parse {
         let statement = match statement {
             Ok(statement) => statement,
@@ -19,4 +26,5 @@ fn main() {
         let statement = constant_fold::statement(statement);
         println!("{statement:?}");
     }
+    */
 }
