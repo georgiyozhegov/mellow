e303c5e073022d5a4292a7e51f201669a328613d
Georgiy Ozhegov <159022025+georgiyozhegov@users.noreply.github.com>
2025-02-15 12:39:43 +0000

    etc: path "source.mellow" by default

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use ir::cfg;
 use syntax;
 
 fn main() {
-    let path = env::args().skip(1).next().unwrap();
+    let path = env::args().skip(1).next().unwrap_or("source.mellow");
     let source = fs::read_to_string(path).unwrap();
     let ast = match syntax::construct(source.chars().peekable()) {
         Ok(ast) => ast,
