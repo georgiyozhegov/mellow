f23b2c294528effc7bef6fee722c8c81f2bce0ed
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2025-02-16 18:16:03 +0400

    etc: omit tests output

--- test/compile.py
+++ test/compile.py
@@ -1,5 +1,8 @@
 import subprocess
+import os
+
+DEV_NULL = open(os.devnull, "w")
 
 def compile(path):
-    output = subprocess.check_output(f"cargo run -- {path}", shell=True)
+    output = subprocess.check_output(f"cargo run -- {path}", shell=True, stderr=DEV_NULL)
     return output.decode("utf-8")
