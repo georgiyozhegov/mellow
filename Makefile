9e4341ea2d39dd5f475dd6fba436cf5a9bf4ff9a
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2025-03-06 14:08:31 +0400

    add: link standard library to assembly output

---
+++ Makefile
@@ -0,0 +1,9 @@
+compile:
+	cargo r > output/main.asm
+	make -C sl compile
+	nasm -f elf64 output/main.asm -o output/main.o
+	gcc -nostdlib -no-pie output/main.o sl/output/main.o -o output/main
+
+clean:
+	make -C sl clean
+	rm output/*
---
+++ sl/debug.c
@@ -0,0 +1,53 @@
+#include "debug.h"
+
+extern void write_c(char value) {
+      __asm__ (
+            "mov $1, %%rax\n"
+            "mov $1, %%rdi\n"
+            "lea %0, %%rsi\n"
+            "mov $1, %%rdx\n"
+            "syscall\n"
+            : : "m"(value)
+      );
+}
+
+extern void debug_c(char value) {
+      write_c(value);
+      write_c('\n');
+}
+
+extern void write_i64(long long value) {
+      char buffer[20];
+      int size = 0;
+      int negative = value < 0;
+
+      if (negative)
+            value = -value;
+
+      while (value > 0) {
+            int digit = value % 10;
+            buffer[size++] = digit + '0';
+            value = (value - digit) / 10;
+      }
+
+      if (negative)
+            buffer[size] = '-';
+
+      for (int index = size; index >= 0; index--)
+            write_c(buffer[index]);
+}
+
+extern void debug_i64(long long value) {
+      write_i64(value);
+      write_c('\n');
+}
+
+extern void write_s(char *value) {
+      for (char *c = value; *c != 0; c++)
+            write_c(*c);
+}
+
+extern void debug_s(char *value) {
+      write_s(value);
+      write_c('\n');
+}
