e4c78c56dd2406ed93b106a33974a113f1adb0b0
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2025-03-07 21:50:14 +0400

    fix: remove "output" folder from gitignore

---
+++ output/main.asm
@@ -0,0 +1,31 @@
+section .bss
+a: resq 1
+section .text
+global _start:
+_start:
+extern debug_i64
+_0:
+mov rbx, 1
+mov [a], rbx
+_1:
+mov rcx, [a]
+mov rbx, 10
+cmp rcx, rbx
+mov rbx, 0
+setl bl
+cmp rbx, 1
+je _2
+jmp _3
+_2:
+mov rbx, [a]
+mov rdi, rbx
+call debug_i64
+mov rbx, [a]
+mov rcx, 1
+add rbx, rcx
+mov [a], rbx
+jmp _1
+_3:
+mov rax, 60
+mov rdi, 0
+syscall
